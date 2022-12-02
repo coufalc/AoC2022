e393d26861a2a840470fe1e1ca369593165e46db
coufalc <coufalca@gmail.com>
2022-12-01 20:57:35 -0800

    Initial commit

---
+++ code/day1.py
@@ -0,0 +1,27 @@
+def day_1():
+    with open('../day1') as infile:
+        groups = infile.read().split('\n\n')
+    max = [(0,0),(-1,-1),(-2,-2)]
+    group = 0
+    for g in groups:
+        group += 1
+        sum = 0
+        for l in g.split('\n'):
+            v=0
+            if l:
+                v = int(l)
+            sum += v
+        #print(group,sum,max)
+        for m in range(len(max)):
+            if sum > max[m][1]:
+                max.insert(m,(group,sum))
+                max = max[:3]
+                break
+        #if group > 5:
+        #    break
+
+    print(f'part 1 answer: {max[0][1]}')
+    total = 0
+    for m in max:
+        total += m[1]
+    print(f'part 2 answer: {total}')
---
+++ main.py
@@ -0,0 +1,4 @@
+import code
+
+if __name__ == '__main__':
+    
